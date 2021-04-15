841895d73bfda33f05c688722776b3637c674366
snowty-developer <snowty.player@gmail.com>
2021-04-15 16:52:23 +0300

    Initial Commit

---
+++ TestForWebSolutions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForWebSolutions
+//
+//  Created by Александр Зубарев on 15.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
